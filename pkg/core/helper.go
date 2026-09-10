b56d703911b019770a9929affa61a5dd7b5da826
Shubham Bhadoria <shubham.bhadoria@netcracker.com>
2026-09-10 14:53:38 +0530

    fix: removed logs

--- pkg/core/helper.go
+++ pkg/core/helper.go
@@ -375,8 +375,6 @@ func (r *DefaultKubernetesHelperImpl) PatchPVCAnnotations(name, namespace string
 }
 
 func (r *DefaultKubernetesHelperImpl) ExpandPVC(pvc *v1.PersistentVolumeClaim) (bool, error) {
-	logger := GetLogger(getEnvAsBool("DEBUG_LOG", true))
-	logger.Info("Expand PVC func")
 	foundPvc := &v1.PersistentVolumeClaim{}
 	err := r.Client.Get(context.TODO(), types.NamespacedName{Name: pvc.Name, Namespace: pvc.Namespace}, foundPvc)
 	if errors.IsNotFound(err) {
@@ -428,43 +426,37 @@ func (r *DefaultKubernetesHelperImpl) ExpandPVC(pvc *v1.PersistentVolumeClaim) (
 	}
 
 	resizeInProgress := desiredSize.Cmp(currentSize) > 0
-	logger.Info("Resize done ")
 	return resizeInProgress, r.Client.Update(context.TODO(), foundPvc)
 }
 
 func (r *DefaultKubernetesHelperImpl) WaitForPVCExpansion(pvcName, namespace string, waitSeconds int) (bool, error) {
-    logger := GetLogger(getEnvAsBool("DEBUG_LOG", true))
-    logger.Info("WaitForPVCExpansion ----")
-
-    var needsRestart bool
-    err := wait.PollImmediate(2*time.Second, time.Duration(waitSeconds)*time.Second,
-        func() (bool, error) {
-            pvc := &v1.PersistentVolumeClaim{}
-            if err := r.Client.Get(context.TODO(), types.NamespacedName{Name: pvcName, Namespace: namespace}, pvc); err != nil {
-                return false, err
-            }
-            if pvc.Status.Phase != v1.ClaimBound {
-                return false, nil
-            }
-            for _, cond := range pvc.Status.Conditions {
-                if cond.Type == v1.PersistentVolumeClaimFileSystemResizePending &&
-                    cond.Status == v1.ConditionTrue {
-                    needsRestart = true
-                    return true, nil
-                }
-            }
-            requested := pvc.Spec.Resources.Requests[v1.ResourceStorage]
-            capacity := pvc.Status.Capacity[v1.ResourceStorage]
-            if capacity.Cmp(requested) >= 0 {
-                return true, nil
-            }
-            return false, nil
-        },
-    )
-    logger.Sugar().Infof("wait seconds %v", waitSeconds)
-    logger.Info("reached here at last")
-    return needsRestart, err
-} 
+	var needsRestart bool
+	err := wait.PollImmediate(2*time.Second, time.Duration(waitSeconds)*time.Second,
+		func() (bool, error) {
+			pvc := &v1.PersistentVolumeClaim{}
+			if err := r.Client.Get(context.TODO(), types.NamespacedName{Name: pvcName, Namespace: namespace}, pvc); err != nil {
+				return false, err
+			}
+			if pvc.Status.Phase != v1.ClaimBound {
+				return false, nil
+			}
+			for _, cond := range pvc.Status.Conditions {
+				if cond.Type == v1.PersistentVolumeClaimFileSystemResizePending &&
+					cond.Status == v1.ConditionTrue {
+					needsRestart = true
+					return true, nil
+				}
+			}
+			requested := pvc.Spec.Resources.Requests[v1.ResourceStorage]
+			capacity := pvc.Status.Capacity[v1.ResourceStorage]
+			if capacity.Cmp(requested) >= 0 {
+				return true, nil
+			}
+			return false, nil
+		},
+	)
+	return needsRestart, err
+}
 
 func (r *DefaultKubernetesHelperImpl) GetStatefulSetByName(name, namespace string) (*v14.StatefulSet, error) {
 	ss := &v14.StatefulSet{}
